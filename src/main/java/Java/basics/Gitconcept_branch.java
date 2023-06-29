db840b46dc241e77d96408fdbba6b0538b0d7c1a
courese <maha2161996@gmail.com>
2023-06-29 20:46:39 +0530

    gitbranch

---
+++ src/main/java/Java/basics/Gitconcept_branch.java
@@ -0,0 +1,17 @@
+package Java.basics;
+
+public class Gitconcept_branch {
+	public void git()
+	{
+		System.out.println("trying for git branch");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gitconcept_branch s = new Gitconcept_branch();
+		s.git();
+
+
+	}
+
+}
