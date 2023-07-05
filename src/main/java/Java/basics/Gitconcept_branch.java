7fe1e6e30a6737a032dd167242a0c54e558013c1
courese <maha2161996@gmail.com>
2023-07-05 21:09:12 +0530

    gitbranch

--- src/main/java/Java/basics/Gitconcept_branch.java
+++ src/main/java/Java/basics/Gitconcept_branch.java
@@ -5,11 +5,16 @@ public class Gitconcept_branch {
 	{
 		System.out.println("trying for git branch");
 	}
+	public void method() {
+		System.out.println("*****");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gitconcept_branch s = new Gitconcept_branch();
 		s.git();
+		s.method();
 
 
 	}
